0323628fef378d48c54f6b7a3ff066b82687f399
Hadi <kargah_iut1403>
2025-03-11 01:04:08 +0330

    text

---
+++ prime.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int is_prime(int num) {
+    if (num < 2) return 0;
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0)
+            return 0;
+    }
+    return 1;
+}
+
+int main() {
+    int num;
+    printf("یک عدد وارد کنید: ");
+    scanf("%d", &num);
+
+    if (is_prime(num))
+        printf("%d یک عدد اول است.\n", num);
+    else
+        printf("%d عدد اول نیست.\n", num);
+
+    return 0;
+}
