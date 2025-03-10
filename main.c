8251bc0a2826d2eadbe48a43a640c5b960b55c2c
kooshakazemig@gmail.com <kooshakazemig@gmail.com>
2025-03-10 21:15:32 +0000

    update and create counter of digits numbers

--- main.c
+++ main.c
@@ -1,10 +1,24 @@
 #include <stdio.h>
 
+int CounterOfDigits(int number){
+
+	int counter = 0;
+	while(counter/10)
+		counter++;
+	return counter;
+
+
+
+
+}
+
+
+
 int main(){
 	int number;
 
-	print("enter the number : ");
+	printf("enter the number : ");
 	scanf("%d", &number);
-	printf(number);
+	printf("%d",number);
 	return 0;
 }
